cb2fc0d9465f182f7b0d40b86afa29a229f82fb4
amirpython <pascalazubi@gmail.com>
2019-10-11 14:11:52 +0200

    fixed html value name

--- cryptocurrency_portfolio/main.py
+++ cryptocurrency_portfolio/main.py
@@ -5,24 +5,20 @@ from flask import request
 import requests
 import json
 
+app = Flask("Cryptocurrency Portfolio") 
+
 # API von coinmarketcap.com
 globalURL = "https://api.coinmarketcap.com/v1/global/"
 tickerURL = "https://api.coinmarketcap.com/v1/ticker/"
 
-# Daten aus der globalURL
+# Daten aus der globalURL beziehen
 request = requests.get(globalURL)
 data = request.json()
 globalMarketCap = data['total_market_cap_usd']
-
-app = Flask("Cryptocurrency Portfolio")
     
-@app.route("/portfolio/", methods=['GET', 'POST'])
-def hallo():
-    if request.method == 'POST':
-        ziel_person = request.form['vorname']
-        rueckgabe_string = "Gesamte Marktkapitalisierung: " + globalMarketCap
-        return rueckgabe_string
-    return render_template("index.html")
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template("index.html", globalMarketCap=globalMarketCap)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
