73d4f3f138f48204c960f71c42c4544f42249e67
amirpython <pascalazubi@gmail.com>
2019-10-11 13:59:32 +0200

    deleted some code, just need API link

--- cryptocurrency_portfolio/main.py
+++ cryptocurrency_portfolio/main.py
@@ -5,51 +5,14 @@ from flask import request
 import requests
 import json
 
-while True:
-    # base URLs
-    globalURL = "https://api.coinmarketcap.com/v1/global/"
-    tickerURL = "https://api.coinmarketcap.com/v1/ticker/"
+# API von coinmarketcap.com
+globalURL = "https://api.coinmarketcap.com/v1/global/"
+tickerURL = "https://api.coinmarketcap.com/v1/ticker/"
 
-    # get data from globalURL
-    request = requests.get(globalURL)
-    data = request.json()
-    globalMarketCap = data['total_market_cap_usd']
-
-    # menu
-    print()
-    print("Welcome to the CoinMarketCap Explorer!")
-    print("Global cap of all cryptocurrencies: $" + str(globalMarketCap))
-    print("Enter 'all' or 'name of crypto' (i.e. bitcoin) to see the name of the top 100 currencies or a specific currency")
-    print()
-    choice = input("Your choice: ")
-
-    if choice == "all":
-        request = requests.get(tickerURL)
-        data = request.json()
-
-        for x in data:
-            ticker = x['symbol']
-            price = x['price_usd']
-
-            print(ticker + ":\t\t$" + price)
-        print()
-
-    else:
-        tickerURL += '/'+choice+'/'
-        request = requests.get(tickerURL)
-        data = request.json()
-
-        ticker = data[0]['symbol']
-        price = data[0]['price_usd']
-
-        print(ticker + ":\t\t$" + price)
-        print()
-
-    choice2 = input("Again? (y/n): ")
-    if choice2 == "y":
-        continue
-    if choice2 == "n":
-        break
+# Daten aus der globalURL
+request = requests.get(globalURL)
+data = request.json()
+globalMarketCap = data['total_market_cap_usd']
 
 app = Flask("Cryptocurrency Portfolio")
     
@@ -57,12 +20,9 @@ app = Flask("Cryptocurrency Portfolio")
 def hallo():
     if request.method == 'POST':
         ziel_person = request.form['vorname']
-        rueckgabe_string = "Hello " + ziel_person + "!"
+        rueckgabe_string = "Gesamte Marktkapitalisierung: " + globalMarketCap
         return rueckgabe_string
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-    
-    
-global_url =
