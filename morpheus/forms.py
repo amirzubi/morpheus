8771947d20ba09a327f8a31d32be0d421fbf0dd9
amirpython <pascalazubi@gmail.com>
2019-10-14 20:46:34 +0200

    added register/login/positions/database/some css fixes

---
+++ morpheus/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Nutzername',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('E-Mail',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Passwort', validators=[DataRequired()])
+    confirm_password = PasswordField('Passwort bestätigen',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Registrieren')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('E-Mail',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Passwort', validators=[DataRequired()])
+    remember = BooleanField('Anmeldedaten merken')
+    submit = SubmitField('Login')
--- morpheus/main.py
+++ morpheus/main.py
@@ -1,56 +1,79 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
 from datetime import datetime
-import requests
-import json
 
-app = Flask("Morpheus")
-
-###### API DATEN
-
-### API von coinmarketcap.com
-globalURL = "https://api.coinmarketcap.com/v1/global/"
-tickerURL = "https://api.coinmarketcap.com/v1/ticker/"
-
-### GLOBAL URL
-# Daten aus globalURL beziehen
-request_globalURL = requests.get(globalURL)
-data_globalURL = request_globalURL.json()
-# Daten aus globalURL definieren
-global_marketcap = data_globalURL['total_market_cap_usd']
-
-### TICKER URL
-# Daten aus tickerURL beziehen
-request_tickerURL = requests.get(tickerURL)
-data_tickerURL = request_tickerURL.json()
-# Daten aus tickerURL definieren
-id = data_tickerURL[0]['id']
-name = data_tickerURL[0]['name']
-symbol = data_tickerURL[0]['symbol']
-rank = data_tickerURL[0]['rank']
-price = data_tickerURL[0]['price_usd']
-price_btc = data_tickerURL[0]['price_btc']
-market_cap_usd = data_tickerURL[0]['market_cap_usd']
-percent_change_1h = data_tickerURL[0]['percent_change_1h']
-percent_change_24h = data_tickerURL[0]['percent_change_24h']
-last_updated = data_tickerURL[0]['last_updated']
-
-##### PORTFOLIO
-# Portfolio Daten
-portfolio_value = 0.00
-last_updated = 0
-
-# with open("portfolio.json") as portfolio:
- #   read_content = json.load(portfolio)
-# portfolio_name = portfolio['id']
-# portfolio_amount = portfolio['amount']
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8bf453d6ee5d32678889844e83bb649f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 
-@app.route("/", methods=['GET', 'POST'])
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    positions = db.relationship('Position', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Position(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    coin = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Position('{self.coin}', '{self.date_posted}')"
+
+
+positions = [
+    {
+        'author': 'Pascal',
+        'coin': 'Bitcoin',
+        'amount': 1.5,
+        'date_posted': '20. Oktober 2019'
+    },
+    {
+        'author': 'Pascal',
+        'coin': 'Ethereum',
+        'amount': 5.5,
+        'date_posted': '21. Oktober 2019'
+    }
+]
+
+
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template("index.html", global_marketcap=global_marketcap )
+    return render_template('index.html', positions=positions)
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Der Account mit dem Nutzernamen {form.username.data} ist erstellt worden.', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Registrieren', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'info@pazu.ch' and form.password.data == 'password':
+            flash('Du bist nun eingeloggt.', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Ups! Etwas ist schiefgelaufen. Bitte überprüfe Deine E-Mail oder Dein Passwort.', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
